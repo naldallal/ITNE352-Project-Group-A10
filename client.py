3a91f9aa34395bc5a72351084df3cdf36c6a44a8
noor-ashoor <noorashoor2004@gmail.com>
2024-12-07 19:19:41 +0300

    gui touches

--- client.py
+++ client.py
@@ -12,64 +12,80 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect to the server
 def connect_to_server():
     client_socket.connect((SERVER_IP, SERVER_PORT))
+# Connect to the server
+connect_to_server()
 
 # Main execution
 root = tk.Tk()
 root.title("News Client")
-root.geometry("470x340" )
-
+root.geometry("500x600")
 
 # Ask for the username
 username = simpledialog.askstring("Input", "Enter your name:")
+# Send username to the server
+client_socket.sendall(username.encode('utf-8'))
+
+# Receive and show greeting message from the server
+greeting = client_socket.recv(1024).decode('utf-8')
+
+# Create the main frame
+main_frame = tk.Frame(root)
+main_frame.pack(fill=tk.BOTH, expand=True)
+
 
 # Main function to create the GUI
 def create_widgets():
-    # Create the main menu
-    main_menu = tk.Frame(root)
-    main_menu.pack()
+    # Clear the main frame first
+    for widget in main_frame.winfo_children():
+        widget.destroy()
 
-    label = tk.Label(main_menu, text="Main Menu" , font="Calibre 20 bold" )
-    label.pack(pady="13")
+    label = tk.Label(main_frame, text="Main Menu", font="Calibre 20 bold")
+    label.pack(pady=10)
 
-    search_button = tk.Button(main_menu, text="Search Headlines", command=search_headlines , font="Calibre 13 bold" , padx="10", pady="13")
-    search_button.pack(pady="13")
+    search_button = tk.Button(main_frame, text="Search Headlines", command=search_headlines, font="Calibre 13 bold", padx=10, pady=10)
+    search_button.pack(pady=10)
 
-    list_sources_button = tk.Button(main_menu, text="List of Sources", command=list_sources , font="Calibre 13 bold" , padx="10", pady="13")
-    list_sources_button.pack(pady="13")
+    list_sources_button = tk.Button(main_frame, text="List of Sources", command=list_sources, font="Calibre 13 bold", padx=10, pady=10)
+    list_sources_button.pack(pady=10)
 
-    quit_button = tk.Button(main_menu, text="Quit", command=quit_app , font="Calibre 13 bold" , padx="10", pady="13")
-    quit_button.pack(pady="13")
+    quit_button = tk.Button(main_frame, text="Quit", command=quit_app, font="Calibre 13 bold", padx=10, pady=10)
+    quit_button.pack(pady=10)
 
 
-# Handle opening a window for searching headlines
+# Function to update the main frame with new content
+def update_frame(content_func):
+    for widget in main_frame.winfo_children():
+        widget.destroy()
+    content_func()
+
+# searching headlines
 def search_headlines():
-    headlines_window = tk.Toplevel(root)
-    headlines_window.title("Search Headlines")
-    
-    label = tk.Label(headlines_window, text="Choose an option:" , font="Calibre 13 bold" , padx="10", pady="13")
-    label.pack(pady="13")
+    update_frame(search_headlines_content)
 
-    keyword_button = tk.Button(headlines_window, text="Search by Keyword", command=search_by_keyword , font="Calibre 13 bold" , padx="10", pady="13")
-    keyword_button.pack(pady="13")
+def search_headlines_content():
+    label = tk.Label(main_frame, text="Choose an option:", font="Calibre 13 bold", padx=10, pady=10)
+    label.pack(pady=10)
 
-    category_button = tk.Button(headlines_window, text="Search by Category", command=search_by_category , font="Calibre 13 bold" , padx="10", pady="13")
-    category_button.pack(pady="13")
+    keyword_button = tk.Button(main_frame, text="Search by Keyword", command=search_by_keyword, font="Calibre 13 bold", padx=10, pady=10)
+    keyword_button.pack(pady=10)
 
-    country_button = tk.Button(headlines_window, text="Search by Country", command=search_by_country , font="Calibre 13 bold" , padx="10", pady="13")
-    country_button.pack(pady="13")
+    category_button = tk.Button(main_frame, text="Search by Category", command=search_by_category, font="Calibre 13 bold", padx=10, pady=10)
+    category_button.pack(pady=10)
 
-    list_all_button = tk.Button(headlines_window, text="List All Headlines", command=list_all_headlines , font="Calibre 13 bold" , padx="10", pady="13")
-    list_all_button.pack(pady="13")
+    country_button = tk.Button(main_frame, text="Search by Country", command=search_by_country, font="Calibre 13 bold", padx=10, pady=10)
+    country_button.pack(pady=10)
 
-    back_button = tk.Button(headlines_window, text="Back to Main Menu", command=headlines_window.destroy , font="Calibre 13 bold" , padx="10", pady="13")
-    back_button.pack(pady="13")
+    list_all_button = tk.Button(main_frame, text="List All Headlines", command=list_all_headlines, font="Calibre 13 bold", padx=10, pady=10)
+    list_all_button.pack(pady=10)
+
+    back_button = tk.Button(main_frame, text="Back to Main Menu", command=create_widgets, font="Calibre 13 bold", padx=10, pady=10)
+    back_button.pack(pady=10)
 
 # Handle searching headlines by keyword
 def search_by_keyword():
     keyword = simpledialog.askstring("Input", "Enter the keyword:")
     client_socket.sendall(f"headline-keyword-{keyword}".encode('utf-8'))
     response = client_socket.recv(4096).decode('utf-8')
-    show_message(response)
     show_results(response)
 
 # Handle searching headlines by category
@@ -77,7 +93,6 @@ def search_by_category():
     category = simpledialog.askstring("Input", "Enter category [ business, general, health, science, sports, technology ]:")
     client_socket.sendall(f"headline-category-{category}".encode('utf-8'))
     response = client_socket.recv(4096).decode('utf-8')
-    show_message(response)
     show_results(response)
 
 # Handle searching headlines by country
@@ -85,53 +100,49 @@ def search_by_country():
     country = simpledialog.askstring("Input", "Enter a country [au, ca, jp, ae, sa, kr, us, ma]:")
     client_socket.sendall(f"headline-country-{country}".encode('utf-8'))
     response = client_socket.recv(4096).decode('utf-8')
-    show_message(response)
     show_results(response)
 
 # Handle listing all headlines
 def list_all_headlines():
     client_socket.sendall(b"headline-all")
     response = client_socket.recv(4096).decode('utf-8')
-    show_message(response)
     show_results(response)
 
 # Handle listing sources
 def list_sources():
-    sources_window = tk.Toplevel(root)
-    sources_window.title("List of Sources")
-    
-    label = tk.Label(sources_window, text="Choose an option:")
-    label.pack(pady="13")
+    update_frame(list_sources_content)
 
-    category_button = tk.Button(sources_window, text="Search by Category", command=search_sources_by_category , font="Calibre 13 bold" , padx="10", pady="13")
-    category_button.pack(pady="13")
+def list_sources_content():
+    label = tk.Label(main_frame, text="Choose an option:", font="Calibre 13 bold", padx=10, pady=10)
+    label.pack(pady=10)
 
-    country_button = tk.Button(sources_window, text="Search by Country", command=search_sources_by_country , font="Calibre 13 bold" , padx="10", pady="13")
-    country_button.pack(pady="13")
+    category_button = tk.Button(main_frame, text="Search by Category", command=search_sources_by_category, font="Calibre 13 bold", padx=10, pady=10)
+    category_button.pack(pady=10)
 
-    language_button = tk.Button(sources_window, text="Search by Language", command=search_sources_by_language, font="Calibre 13 bold" , padx="10", pady="13")
-    language_button.pack(pady="13")
+    country_button = tk.Button(main_frame, text="Search by Country", command=search_sources_by_country, font="Calibre 13 bold", padx=10, pady=10)
+    country_button.pack(pady=10)
 
-    list_all_button = tk.Button(sources_window, text="List All", command=list_all_sources, font="Calibre 13 bold" , padx="10", pady="13")
-    list_all_button.pack(pady="13")
+    language_button = tk.Button(main_frame, text="Search by Language", command=search_sources_by_language, font="Calibre 13 bold", padx=10, pady=10)
+    language_button.pack(pady=10)
 
-    back_button = tk.Button(sources_window, text="Back to Main Menu", command=sources_window.destroy , font="Calibre 13 bold" , padx="10", pady="13")
-    back_button.pack(pady="13")
+    list_all_button = tk.Button(main_frame, text="List All", command=list_all_sources, font="Calibre 13 bold", padx=10, pady=10)
+    list_all_button.pack(pady=10)
+
+    back_button = tk.Button(main_frame, text="Back to Main Menu", command=create_widgets, font="Calibre 13 bold", padx=10, pady=10)
+    back_button.pack(pady=10)
 
 # Handle searching sources by category
 def search_sources_by_category():
     category = simpledialog.askstring("Input", "Enter category [ business, general, health, science, sports, technology ]:")
     client_socket.sendall(f"source-category-{category}".encode('utf-8'))
     response = client_socket.recv(4096).decode('utf-8')
-    show_message(response)
     show_results(response)
 
 # Handle searching sources by country
 def search_sources_by_country():
-    country = simpledialog.askstring("Input", "Enter country [ au, ca, jp, ae, sa, kr, us, ma ]:" )
+    country = simpledialog.askstring("Input", "Enter country [ au, ca, jp, ae, sa, kr, us, ma ]:")
     client_socket.sendall(f"source-country-{country}".encode('utf-8'))
     response = client_socket.recv(4096).decode('utf-8')
-    show_message(response)
     show_results(response)
 
 # Handle searching sources by language
@@ -139,57 +150,46 @@ def search_sources_by_language():
     language = simpledialog.askstring("Input", "Enter language [ ar, en ]:")
     client_socket.sendall(f"source-language-{language}".encode('utf-8'))
     response = client_socket.recv(4096).decode('utf-8')
-    show_message(response)
     show_results(response)
 
 # Handle listing all sources
 def list_all_sources():
     client_socket.sendall(b"source-all")
     response = client_socket.recv(4096).decode('utf-8')
-    show_message(response)
     show_results(response)
 
 # Handle showing results
 def show_results(response):
-    results = response.split('\n')
-    results_window = tk.Toplevel(root)
-    results_window.title("Results")
+    for widget in main_frame.winfo_children():
+        widget.destroy()
+    
+    response_text = tk.Text(main_frame, height=15, width=50)
+    response_text.pack()
+    response_text.insert(tk.END, f"{response}\n")
 
-    num_topic = simpledialog.askfloat("Input", "Select number of topics you are interested in for more details:")
+    article_number = simpledialog.askstring("Input", "Enter the article number for details (or 'exit' to go back):")
+    if article_number.lower() != 'exit':
+        # Clear the response before showing the article details
+        response_text.delete(1.0, tk.END)  
+        client_socket.sendall(article_number.encode('utf-8'))
+        article_details = client_socket.recv(4096).decode('utf-8')
+        response_text.insert(tk.END, f"\nArticle Details: {article_details}\n")
 
-    # Convert the float to an integer
-    num_topic = int(num_topic)
-
-    client_socket.sendall(str(num_topic).encode())
-    response = client_socket.recv(4096).decode('utf-8')
-    show_message(response)
-
-    back_button = tk.Button(results_window, text="Back to Main Menu", command=results_window.destroy)
+    back_button = tk.Button(main_frame, text="Back to Main Menu", command=create_widgets, font="Calibre 13 bold", padx=10, pady=10)
     back_button.pack()
 
+
+# Handle quitting the app
+def quit_app():
+    client_socket.sendall(b'quit')
+    client_socket.close()
+    root.quit()
 
 # Display message in a message box
 def show_message(message):
     messagebox.showinfo("Response", message)
-
-# Handle quitting the app
-def quit_app():
-    client_socket.sendall(b'Quit')
-    client_socket.close()
-    root.quit()
-
-# Connect to the server
-connect_to_server()
-
-# Send username to the server
-client_socket.sendall(username.encode('utf-8'))
-
-# Receive and show greeting message from the server
-greeting = client_socket.recv(1024).decode('utf-8')
-show_message(greeting)
+show_message(greeting)    
 
 # Set up the main menu
 create_widgets()
-
-# Run the Tkinter event loop
 root.mainloop()
